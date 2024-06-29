791cf3cf6745478eaf3b19a88df27d745cf43d25
aldifaadit75 <aldifaadit75@gmail.com>
2024-06-29 09:16:51 +0000

    adding splashscreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:umkm/home/homeview.dart';
 import 'package:umkm/login/loginpage.dart';
+import 'package:umkm/splash/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: SplashScreen(),
     );
   }
 }
